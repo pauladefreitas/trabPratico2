bf4b8b9990027720af57971283e93220368f98c7
Paula <paulacamargos26@gmail.com>
2022-11-30 17:24:46 -0300

    quaseFinal

--- scripts.js
+++ scripts.js
@@ -1,67 +1,62 @@
     fetch ('https://api.rawg.io/api/games?key=faf5f98c2363424cb9782e3a65276837')
     .then(res => res.json())
     .then(data => {
-        console.log(data.results);
-        let games = document.getElementById('games');
-        let array = new Array();
 
-        for(let i=0; i<6; i++){
-          let j = Math.floor(Math.random() * 20);
-          for(let k=0;k<array.length;k++){
-            if(j===array[k]){
-              j = Math.floor(Math.random() * 20);
-            }
-          }
+        console.log(data.results);
+
+        let games = document.getElementById('games');
+
+
+        for (let i=0; i<6; i++){
+
           games.innerHTML+=`<div class="card mb-3">
 
-            <img class="card-img-top" src="${data.results[j].background_image}"/>
-          <div class="card-body">
-              <h2 class="card-title h4"><b>${data.results[j].name}</b></h2>
-              <p class="card-text"><b>Classificação Metacritic: </b>${data.results[j].metacritic}<br>
-              <b>Data de lançamento: </b>${data.results[j].released} <br>
-              <b>Avaliação: </b>${data.results[j].rating} 
-              </p>
-      
-              <a class="btn btn-primary" href="#detalhes.html">Read more →</a>
-              <br>
-          </div>`
-          
-          array.push(j);
-        }
+          <img class="card-img-top" src="${data.results[i].background_image}"/>
+        <div class="card-body">
+            <h2 class="card-title h4"><b>${data.results[i].name}</b></h2>
+            <p class="card-text"><b>Classificação Metacritic: </b>${data.results[i].metacritic}<br>
+            <b>Data de lançamento: </b>${data.results[i].released} <br>
+            <b>Avaliação: </b>${data.results[i].rating} 
+            </p>
+    
+            <a class="btn btn-primary" href="detalhes.html?id=${data.results[i].id}">Ler mais →</a>
+            <br>
+        </div>`
+          }
+        
     });
+  
 
-    fetch ('https://api.rawg.io/api/developers?key=faf5f98c2363424cb9782e3a65276837')
+    fetch ('https://api.rawg.io/api/platforms?key=faf5f98c2363424cb9782e3a65276837')
     .then(res => res.json())
     .then(data => {
         console.log(data.results);
+
         let plataformas = document.getElementById('plataformas');
-        let array = new Array();
+
 
         for(let i=0; i<3; i++){
-            let j = Math.floor(Math.random() * 20);
-            for(let k=0;k<array.length;k++){
-              if(j===array[k]){
-                j = Math.floor(Math.random() * 20);
-              }
-            }
 
+          plataformas.innerHTML+=`
+          
+          <div class="card mb-3">
 
-          plataformas.innerHTML+=`<div class="card mb-3">
+            <img class="card-img-top" src="${data.results[i].image_background}">
+            <div class="card-body">
+                <h2 class="card-title h4"><b>${data.results[i].name}</b></h2>
+                <p class="card-text"><b>Itens populares:</b> ${data.results[i].games_count}</p>
+                <b>Principais jogos:</b>
+                <ul>
+                  <li>${data.results[i].games[0].name}</li>
+                  <li>${data.results[i].games[1].name}</li>
+                  <li>${data.results[i].games[2].name}</li>
+                </ul>
+            </div>
 
-          <img class="card-img-top" src="${data.results[j].image_background}">
-          <div class="card-body">
-              <h2 class="card-title h4">${data.results[j].name}</h2>
-              <p class="card-text">Itens populares: ${data.results[j].games_count}</p>
-          </div>
-
-      </div>`
+          </div>`
         
-
-          array.push(j);
         }
     });
-
-
 
 
 
