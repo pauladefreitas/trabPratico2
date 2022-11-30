99bb131122b8f7a6b30e8a997c5bbd60263f5955
Paula <paulacamargos26@gmail.com>
2022-11-30 12:41:32 -0300

    trabFinal

--- scripts.js
+++ scripts.js
@@ -1,15 +1,3 @@
-function success () {
-    console.log(JSON.parse(this.responseText));
-}
-function error (err) { console.log('Erro:', err);}
-
-var xhr = new XMLHttpRequest(); 
-xhr.onload = success;
-xhr.onerror = error;
-
-xhr.open('GET', 'https://api.rawg.io/api/platforms?key=faf5f98c2363424cb9782e3a65276837')
-xhr.send(); 
-
     fetch ('https://api.rawg.io/api/games?key=faf5f98c2363424cb9782e3a65276837')
     .then(res => res.json())
     .then(data => {
@@ -26,7 +14,7 @@ xhr.send();
           }
           games.innerHTML+=`<div class="card mb-3">
 
-            <img class="card-img-top" src="${data.results[j].background_image}" alt="..." />
+            <img class="card-img-top" src="${data.results[j].background_image}"/>
           <div class="card-body">
               <h2 class="card-title h4"><b>${data.results[j].name}</b></h2>
               <p class="card-text"><b>Classificação Metacritic: </b>${data.results[j].metacritic}<br>
@@ -34,42 +22,44 @@ xhr.send();
               <b>Avaliação: </b>${data.results[j].rating} 
               </p>
       
-              <a class="btn btn-primary" href="#!">Read more →</a>
+              <a class="btn btn-primary" href="#detalhes.html">Read more →</a>
               <br>
-          </div>
+          </div>`
           
-          `
           array.push(j);
         }
     });
 
-    fetch ('https://api.rawg.io/api/platforms?key=faf5f98c2363424cb9782e3a65276837')
+    fetch ('https://api.rawg.io/api/developers?key=faf5f98c2363424cb9782e3a65276837')
     .then(res => res.json())
     .then(data => {
         console.log(data.results);
         let plataformas = document.getElementById('plataformas');
         let array = new Array();
 
-        for(let i=0; i<4; i++){
-          let j = 1;
-          j++
+        for(let i=0; i<3; i++){
+            let j = Math.floor(Math.random() * 20);
+            for(let k=0;k<array.length;k++){
+              if(j===array[k]){
+                j = Math.floor(Math.random() * 20);
+              }
+            }
 
 
           plataformas.innerHTML+=`<div class="card mb-3">
 
-          <img class="card-img-top" src="${data.results[j].background_image}" alt="..." />
+          <img class="card-img-top" src="${data.results[j].image_background}">
           <div class="card-body">
               <h2 class="card-title h4">${data.results[j].name}</h2>
               <p class="card-text">Itens populares: ${data.results[j].games_count}</p>
-              <a class="btn btn-primary" href="#!">Read more →</a>
           </div>
 
       </div>`
-        }
+        
 
           array.push(j);
         }
-    );
+    });
 
 
 
